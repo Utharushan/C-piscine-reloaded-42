ba49c7190408bbbbdcda098650ff782c036c153a
Utharushan <Utharushan@gmail.com>
2024-11-04 23:27:30 +0100

    ft_print_params

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 23:20:33 by marvin            #+#    #+#             */
+/*   Updated: 2024/11/04 23:20:33 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+    ft_putchar('\n');
+}
+
+int	main(int argc, char *argv[])
+{
+	int i;
+
+	i = 1;
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			ft_putstr(argv[i]);
+			i++;
+		}
+	}
+}
